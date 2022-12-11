4c6acf7fe61f627b5c04fbc2a52223426313ebd5
Ladino Anselmo <ladino.anselmo@gmail.com>
2022-12-11 12:18:16 +0200

    add responsive sizes

--- lib/flutter_basis.dart
+++ lib/flutter_basis.dart
@@ -9,3 +9,5 @@ export 'src/navigation/transition.dart' show TransitionType;
 export 'src/navigation/routing.dart';
 export 'src/navigation/basis_route.dart';
 export 'src/navigation/widget_args_builder.dart';
+
+export 'src/responsive/responsive.dart';
